ac0a28e7b1aeaa24a96f7289b3bec1d69015fdaf
Jhony benitez <jonathanbenitez_98@hotmail.com>
2024-10-07 09:06:28 -0300

    Update ejercicios.js

--- ejercicios.js
+++ ejercicios.js
@@ -1,26 +1,31 @@
 // Operadores de comparación
 // 1. Comprueba si 10 es mayor o igual que 9 e imprime el resultado en consola.
 function compareTenAndNine() {
-    // console.log(10 >= 9); // Imprime: true
+console.log(10 >= 9); // Imprime: true
 
 }
 
 // 2. Comprueba si 0 es igual a 0 e imprime el resultado en consola.
 function compareZeroAndZero() {
-    // console.log(0 == 0); // Imprime: true
+    console.log(0 == 0); // Imprime: true
 console.log(0 === 0); // Imprime: true
 
 }
 
 // 3. Comprueba si 7 es mayor que 8 y menor que 10, e imprime el resultado en consola.
 function compareSeven() {
-    // Tu código aquí...
+if (7 > 8 && 7 < 10) {
+  console.log("Verdadero");
+} else {
+  console.log("Falso");
+}
+
 }
 
 // Operadores lógicos
 // 4. Verifica si puedes comprar un producto de 1500 con un descuento del 25% teniendo 1150€ e imprime si puedes o no.
 function canBuyProduct() {
-    // console.log(7 > 8 && 7 < 10); // Imprime: false
+    console.log(7 > 8 && 7 < 10); // Imprime: false
 
 
 }
@@ -28,21 +33,21 @@ function canBuyProduct() {
 // Variables
 // 5. Crea una variable llamada `mensaje` y asígnale el valor "Hola JavaScript", luego imprime la variable en consola.
 function createMessageVariable() {
-    // let mensaje = "Hola JavaScript";
+    let mensaje = "Hola JavaScript";
 console.log(mensaje); // Imprime: Hola JavaScript
 
 }
 
 // 6. Crea una variable llamada `resultado` y asígnale la suma de 2 y 3, luego imprime la variable en consola.
 function createSumVariable() {
-    // let resultado = 2 + 3;
+    let resultado = 2 + 3;
 console.log(resultado); // Imprime: 5
 
 }
 
 // 7. Crea una constante llamada `IS_DISABLED` y asígnale el valor booleano `true`, luego imprime la constante en consola.
 function createDisabledConstant() {
-    // const IS_DISABLED = true;
+    const IS_DISABLED = true;
 console.log(IS_DISABLED); // Imprime: true
 
 }
@@ -50,14 +55,14 @@ console.log(IS_DISABLED); // Imprime: true
 // Null y Undefined
 // 8. Crea una variable con `let` llamada `capacidad` y asígnale un valor `null`, luego imprime la variable en consola.
 function createNullVariable() {
-    // let capacidad = null;
+    let capacidad = null;
 console.log(capacidad);
 
 }
 
 // 9. Crea una variable con `let` llamada `dinero` y asígnale un valor `undefined`, luego imprime la variable en consola.
 function createUndefinedVariable() {
-    // let dinero = undefined;
+    let dinero = undefined;
 console.log(dinero);
 
 }
@@ -65,14 +70,14 @@ console.log(dinero);
 // typeof
 // 10. Escribe un código para ver el tipo de una variable llamada `userName` e imprime el tipo de dato en consola.
 function checkUserNameType() {
-    // let userName = "Jhonatan"; // Puedes asignar cualquier valor a la variable
+    let userName = "Jhonatan"; // Puedes asignar cualquier valor a la variable
 console.log(typeof userName);
 
 }
 
 // 11. Asegúrate de que `dogId` es una cadena de texto y luego imprime el tipo de dato en consola.
 function checkDogIdIsString() {
-    // let dogId = "12345"; // Aseguramos que dogId es una cadena de texto
+    let dogId = "12345"; // Aseguramos que dogId es una cadena de texto
 console.log(typeof dogId); // Imprime "string"
 
 }
@@ -80,20 +85,20 @@ console.log(typeof dogId); // Imprime "string"
 // console.log()
 // 12. Imprime un mensaje en la consola utilizando `console.log()`.
 function logMessage() {
-    // console.log("¡Feliz Navidad!");
+    console.log("¡Feliz Navidad!");
 
 }
 
 // 13. Utiliza `console.log()` para imprimir el valor de la variable `edad` que tiene el valor 30.
 function logAge() {
-    // let edad = 30;
+    let edad = 30;
 console.log(edad);
 
 }
 
 // 14. Muestra el mensaje 'La versión de JavaScript es' seguido de la variable `version` con el valor 2024 usando `console.log()`.
 function logVersion() {
-    // let version = 2024;
+    let version = 2024;
 console.log("La versión de JavaScript es " + version);
 
 }
